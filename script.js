b139628b297a149edf133022710b54490f3cea6b
msmsa2001 <msmsa251001@gmail.com>
2024-05-16 12:46:19 +0530

    solved bug

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ var sidenavs = document.getElementById('sidenavs');
                 document.getElementById("sidenavs").style.display = "none";
             });
         });
+
+        var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+function modalBtn() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
